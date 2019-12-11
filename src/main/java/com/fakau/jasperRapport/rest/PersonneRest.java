b2e75f93618a88d8bf13ca823c9840a1626e6a87
Laurent Kafka M <laurentkafka123@gmail.com>
2019-12-11 11:46:41 -0500

    add, create pdf file

--- src/main/java/com/fakau/jasperRapport/rest/PersonneRest.java
+++ src/main/java/com/fakau/jasperRapport/rest/PersonneRest.java
@@ -1,24 +1,39 @@
 package com.fakau.jasperRapport.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fakau.jasperRapport.model.Personne;
 import com.fakau.jasperRapport.service.PersonneService;
+
+import net.sf.jasperreports.engine.JRException;
 
 @RestController
 @RequestMapping("api")
 public class PersonneRest {
 	@Autowired
-	private PersonneService PersonneService;
+	private PersonneService personneService;
 	
-	@GetMapping("/test")
-	public String test() {
-		return "this is a test";
-	}
-	@GetMapping("/print")
+	@GetMapping("/listpdf")
 	public String generatePDF() {
-		return PersonneService.generatePDF();
+		return personneService.generatePDF(personneService.createJasperPrint());
+	}
+	@GetMapping("/direct-print")
+	public String directPrint(@RequestParam("printer") String printerName) {
+		try {
+			return personneService.directPrint(personneService.createJasperPrint(), printerName);
+		} catch (JRException e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+	@GetMapping("/person")
+	public List<Personne> getAllPersonne() {
+		return personneService.getAll();
 	}
 }
--- src/main/java/com/fakau/jasperRapport/service/PersonneService.java
+++ src/main/java/com/fakau/jasperRapport/service/PersonneService.java
@@ -8,6 +8,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.MediaSizeName;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.print.attribute.standard.PrinterName;
+
 import org.omg.CORBA_2_3.portable.OutputStream;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -21,7 +29,10 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPrintServiceExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimplePrintServiceExporterConfiguration;
 
 
 @Service
@@ -53,28 +64,79 @@ public class PersonneService {
 		personnes.add(personne1);
 		personnes.add(personne2);
 		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		personnes.add(personne1);
+		personnes.add(personne2);
+		personnes.add(personne3);
+		
+		
 		return personnes;
 	}
-	public String generatePDF() {
-		String homeDirectory = System.getProperty("user.home");
-		String outPutFile = homeDirectory + File.pathSeparatorChar + "personne_liste.pdf";
+	public JasperPrint createJasperPrint() {
 		JRBeanCollectionDataSource jRBeanCollectionDataSource =new JRBeanCollectionDataSource(getAll());
-		JRDataSource jRDataSource =new JREmptyDataSource();
 		Map<String, Object> parameters=new HashMap<String, Object>();
 		parameters.put("ds", jRBeanCollectionDataSource);
+		
 		try {
-		 InputStream in = getClass().getClassLoader().getResourceAsStream("jasper_rapport/personne_list.jasper");
-		 System.out.println(in.toString());
-		 JasperReport jasperReport= (JasperReport) JRLoader.loadObject(in);
-		 JasperPrint jasperPrint=
-				 JasperFillManager.
-				 fillReport(jasperReport, parameters, jRDataSource);
-		 FileOutputStream outputStream=new FileOutputStream(new File(outPutFile));
-		 JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+		 InputStream inputStream = new ClassPathResource("jasper/person_list.jasper").getInputStream();		 
+		 JasperReport jasperReport= (JasperReport) JRLoader.loadObject(inputStream);
+		 JasperPrint jasperPrint= JasperFillManager.fillReport(jasperReport, parameters, jRBeanCollectionDataSource);
+		 return jasperPrint;
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+	public String generatePDF(JasperPrint jasperPrint) {
+		try {
+			String homeDirectory = System.getProperty("user.home");
+			String outPutFile = homeDirectory +  "/Desktop/personne_liste.pdf";
+			FileOutputStream outputStream=new FileOutputStream(new File(outPutFile));
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+			outputStream.close();
+			return "file created";
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "error: " + e.getMessage();
+			return e.getMessage();
 		}
-		return "file created";
-	}
+	 }
+	public String directPrint(JasperPrint jasperPrint, String printerName) throws JRException {
+		PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+		  printRequestAttributeSet.add(MediaSizeName.ISO_A4);
+		  if (jasperPrint.getOrientationValue() == net.sf.jasperreports.engine.type.OrientationEnum.LANDSCAPE) {
+		   printRequestAttributeSet.add(OrientationRequested.LANDSCAPE);
+		  } else {
+		   printRequestAttributeSet.add(OrientationRequested.PORTRAIT);
+		  }
+		  PrintServiceAttributeSet printServiceAttributeSet = new HashPrintServiceAttributeSet();
+		  printServiceAttributeSet.add(new PrinterName(printerName, null));
+
+		  JRPrintServiceExporter exporter = new JRPrintServiceExporter();
+		  SimplePrintServiceExporterConfiguration configuration = new SimplePrintServiceExporterConfiguration();
+		  configuration.setPrintRequestAttributeSet(printRequestAttributeSet);
+		  configuration.setPrintServiceAttributeSet(printServiceAttributeSet);
+		  configuration.setDisplayPageDialog(false);
+		  configuration.setDisplayPrintDialog(false);
+
+		  exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		  exporter.setConfiguration(configuration);
+          exporter.exportReport();
+          return "";
+	 }
+	
 }
